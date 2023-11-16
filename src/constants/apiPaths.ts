5481a7ca1606976d2707162f98e8044c80e89552
Andrii Gromakov <andriigromakov@gmail.com>
2023-11-16 10:05:59 +0200

    fix: minor FE integration fix

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://rm5iv9sh3g.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://rm5iv9sh3g.execute-api.eu-west-1.amazonaws.com",
+  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
